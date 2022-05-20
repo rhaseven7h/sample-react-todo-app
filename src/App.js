02846fc2b7d5dd3b2b4f2a95ed631eb359e07ae5
Gabriel Medina <rha7.com@gmail.com>
2022-05-20 00:28:00 -0500

    Moved API base endpoint to an injected dep and on its own Config module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import './App.scss';
 import ToDoList from "./ToDoList/ToDoList";
+import PropTypes from "prop-types";
 
-function App() {
-    const toDosEndpoint = process.env.API_BASE
-        ? process.env.API_BASE
-        : 'http://localhost:3001/todos';
+function App(props) {
     return (
         <div className="container mx-auto mt-12">
             <div className="text-center">
@@ -13,9 +11,13 @@ function App() {
                 <hr/>
                 <p className="text-gray-500 my-4 mt-14"><em>Hey! Don't forget the milk!</em></p>
             </div>
-            <ToDoList apiBase={toDosEndpoint}/>
+            <ToDoList apiBase={props.config.apiEndpointBase}/>
         </div>
     );
 }
+
+App.propTypes = {
+    config: PropTypes.object.isRequired
+};
 
 export default App;
